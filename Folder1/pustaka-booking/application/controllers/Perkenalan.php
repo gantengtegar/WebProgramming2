3f856edacd8eb3ded2655bf18b87f3b9deb53597
Your Name <prasetyat508@gmail.com>
2020-10-19 14:39:06 +0700

    first commit

---
+++ Folder1/pustaka-booking/application/controllers/Perkenalan.php
@@ -0,0 +1,11 @@
+<?php 
+class Perkenalan extends CI_Controller{
+	public function index(){
+		echo "<h1>Perkenalkan<h1>";
+		echo "<h2>Nama saya Tegar Prasetya<br>
+			Saya tinggal didaerah Brebes<br>
+			Olahraga yang saya sukai adalah futsal<h2>";
+	}
+}
+
+
---
+++ Folder2/praktikum2/view-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Latihan 1</title>
+<head>
+
+<body>
+    <h2>Halo kawan .. Yuk kita belajar web programming ... !!! <h2><br>
+    Nilai 1 = <?= $nilai1; ?> <br>
+    Nilai 2 = <?= $nilai2; ?> <br>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1." + ".$nilai2." = ".$hasil; ?>
+</body>
+</html>
